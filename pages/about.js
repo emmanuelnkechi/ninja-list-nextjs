e8b6d7719ab166837818eb5772f7f035465a1082
Nkechi Emmanuel <nkechiemmanuel95@gmail.com>
2021-10-26 22:11:29 +0100

    ninja list

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+import Head from "next/head"
+
+const About = () => {
+    return ( 
+        <>
+        <Head>
+          <title>Ninja List | About</title>
+          <meta name="keywords" content="ninjas"></meta>
+        </Head>
+        <div>
+            <h1>About</h1>
+         <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+         lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+         </p>
+         <p>lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+         lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum
+         </p>
+        </div>
+        </>
+     );
+}
+ 
+export default About;
